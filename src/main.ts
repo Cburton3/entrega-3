62dcdaf3d08d8facaaa787e41d6b9354ab96b9f0
Charles Burton <charlie-246@hotmail.co.uk>
2023-07-27 18:38:17 +0200

    main commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,48 @@
-// import "./style.css";
+interface Music-groups {
+    nombre: string;
+    año: number;
+    active: boolean;
+    género: string;
+}
 
-// console.log("Hello Typescript!");
+const theBeatles = {
+    nombre: 'the Beetles',
+    año: 1960,
+    active: true,
+    género: 'Pop Rock',
+};
 
-console.log('hola mundo')
+console.log(`%ctheBeatles, font-size: 18px, background-color: green, font-weight: bold`);
+
+const queen = {
+    nombre: 'queen',
+    año: 1970,
+    active: false;
+    género: 'Rock',
+};
+
+const acdc = {
+    nombre: 'ACDC',
+    año: 1973,
+    active: true;
+    género: 'Hard Rock',
+};
+
+const beethoven = {
+    nombre: 'Ludwig van Beethoven',
+    año: 1770,
+    active: false;
+    género: 'Classic',
+};
+
+const theRollingStones = {
+    nombre: 'the Rolling Stones',
+    año: 1962,
+    active: true;
+    género: 'Rock',
+};
+
+let popRock = theBeatles;
+let rock = queen, theRollingStones;
+let hardRock = acdc;
+let classic = beethoven;
