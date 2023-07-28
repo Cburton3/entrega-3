5f2abfaa62b84458e026e8abf008d1936acedd9c
Charles Burton <charlie-246@hotmail.co.uk>
2023-07-28 16:21:01 +0200

    intento de ejercicio

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-interface Music-groups {
+interface Music {
     nombre: string;
     año: number;
     active: boolean;
     género: string;
 }
+
+const title = 'font-size:18px; background-color:green; font-weight:bold;'
 
 const theBeatles = {
     nombre: 'the Beetles',
@@ -12,37 +14,47 @@ const theBeatles = {
     género: 'Pop Rock',
 };
 
-console.log(`%ctheBeatles, font-size: 18px, background-color: green, font-weight: bold`);
+
+console.log(`%c ${theBeatles.nombre}`, title);
+// "color:red; font-size: 18px"
 
 const queen = {
-    nombre: 'queen',
+    nombre: 'Queen',
     año: 1970,
-    active: false;
+    active: false,
     género: 'Rock',
 };
+
+console.log(`%c ${queen.nombre}`, title);
 
 const acdc = {
     nombre: 'ACDC',
     año: 1973,
-    active: true;
+    active: true,
     género: 'Hard Rock',
 };
+
+console.log(`%c ${acdc.nombre}`, title);
 
 const beethoven = {
     nombre: 'Ludwig van Beethoven',
     año: 1770,
-    active: false;
+    active: false,
     género: 'Classic',
 };
+
+console.log(`%c ${beethoven.nombre}`, title);
 
 const theRollingStones = {
     nombre: 'the Rolling Stones',
     año: 1962,
-    active: true;
+    active: true,
     género: 'Rock',
 };
 
-let popRock = theBeatles;
-let rock = queen, theRollingStones;
-let hardRock = acdc;
-let classic = beethoven;
+console.log(`%c ${theRollingStones.nombre}`, title);
+
+let popRock = [theBeatles];
+let rock = [queen, theRollingStones];
+let hardRock = [acdc];
+let classic = [beethoven];
